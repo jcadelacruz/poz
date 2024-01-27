40e869d3a7b23b5ab8c9bdd4786ac9a9499b5211
jcadelacruz <112672810+jcadelacruz@users.noreply.github.com>
2024-01-27 14:10:55 +0000

    created main class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  public static int tr(){
+    int tickratePerSecond = 1;
+    return tickratePerSecond;  
+  }
+}
